4541e71c79635c06e1197f527c9c4eb5fa0d2aba
Tanmay Deshmukh <tanmay.dn@gmail.com>
2024-03-09 15:36:06 +0100

    fix: publish particle cloud even on force update

--- nav2_amcl/src/amcl_node.cpp
+++ nav2_amcl/src/amcl_node.cpp
@@ -913,9 +913,7 @@ AmclNode::laserReceived(sensor_msgs::msg::LaserScan::ConstSharedPtr laser_scan)
     pf_sample_set_t * set = pf_->sets + pf_->current_set;
     RCLCPP_DEBUG(get_logger(), "Num samples: %d", set->sample_count);
 
-    if (!force_update_) {
-      publishParticleCloud(set);
-    }
+    publishParticleCloud(set);
   }
   if (resampled || force_publication || !first_pose_sent_) {
     amcl_hyp_t best_hyp;
